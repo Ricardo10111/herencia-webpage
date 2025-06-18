6bab62b637e7eac165f207d42e753e2e47ae272a
Ricardo Lopez <albelop1m@gmail.com>
2025-06-18 12:03:36 -0700

    chore: changes on the images

--- components/PortfolioHome.jsx
+++ components/PortfolioHome.jsx
@@ -5,32 +5,7 @@ import FadeIn from './FadeIn'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const portfolioList = [
-  {
-    id: '01',
-    title: 'The Baja Door',
-    description:
-      'Lorem ipsum dolor sit amet consecte Dignissim consectetur tristique purus vehicula felis velit.',
-    image: '/portfolio/image.jpg',
-    image2: '/portfolio/image.jpg',
-  },
-  {
-    id: '02',
-    title: 'Custom Sculpture',
-    description:
-      'Sculpted by hand with detailed woodwork to tell a story in every curve and cut.',
-    image: '/portfolio/image3.jpg',
-    image2: '/portfolio/image3.jpg',
-  },
-  {
-    id: '03',
-    title: 'Artisan Table',
-    description:
-      'Crafted with elegance and care, our tables reflect your home’s spirit.',
-    image: '/portfolio/image4.jpg',
-    image2: '/portfolio/image4.jpg',
-  },
-]
+import woodarts from '@/lib/data/woodarts'
 
 const PortfolioHome = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -41,9 +16,7 @@ const PortfolioHome = () => {
       setFadeState('fade-out') // empieza salida
 
       setTimeout(() => {
-        setCurrentIndex((prev) =>
-          prev === portfolioList.length - 1 ? 0 : prev + 1,
-        )
+        setCurrentIndex((prev) => (prev === woodarts.length - 1 ? 0 : prev + 1))
         setFadeState('fade-in')
       }, 700)
     }, 6000)
@@ -51,7 +24,7 @@ const PortfolioHome = () => {
     return () => clearInterval(interval)
   }, [])
 
-  const item = portfolioList[currentIndex]
+  const woodart = woodarts[currentIndex]
 
   return (
     <section className='w-full'>
@@ -67,16 +40,16 @@ const PortfolioHome = () => {
       <div
         className={`transition-container ${fadeState} flex flex-col md:flex-row items-center justify-between gap-12 px-6 w-full`}
       >
-        <div className='bg-[#F8F8F8] max-w-[572px] p-10 max-h-[323px] flex flex-col justify-center mt-40'>
-          <h4 className='text-5xl font-bold text-[#c5a95e]'>{item.id}</h4>
-          <h3 className='text-3xl text-[#4a4a4a] mb-3'>{item.title}</h3>
-          <p className='text-[#5B5B5B] leading-relaxed'>{item.description}</p>
+        <div className='bg-[#F8F8F8] max-w-[572px] md:w-[600px] p-10 max-h-[323px] flex flex-col justify-center mt-40'>
+          <h4 className='text-5xl font-bold text-[#c5a95e]'>{woodart.id}</h4>
+          <h3 className='text-3xl text-[#4a4a4a] mb-3'>{woodart.title}</h3>
+          <p className='text-[#5B5B5B] leading-relaxed'>{woodart.feature}</p>
         </div>
 
         <div className='relative w-[320px] h-[492px]'>
           <Image
-            src={item.image}
-            alt={item.title}
+            src={woodart.image}
+            alt={woodart.title}
             layout='fill'
             objectFit='cover'
             className='shadow-md'
@@ -85,8 +58,8 @@ const PortfolioHome = () => {
 
         <div className='relative w-[320px] h-[492px]'>
           <Image
-            src={item.image2}
-            alt={`${item.title} detail`}
+            src={woodart.image2}
+            alt={`${woodart.title} detail`}
             layout='fill'
             objectFit='cover'
             className='shadow-md'
